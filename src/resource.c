4f54b0780c5f5e9c04d4ae951d43703f2e3d85d6
Jens Nyberg <jens.nyberg@gmail.com>
2020-05-13 02:06:30 +0200

    more work
    Signed-off-by: Jens Nyberg <jens.nyberg@gmail.com>

--- src/resource.c
+++ src/resource.c
@@ -72,63 +72,115 @@ static unsigned int resolve(struct resource *resource, const char *name, char * 
 
 }
 
-static unsigned int naviblank(struct resource *resource, unsigned int count, void *data)
+static unsigned int _curl_match(struct resource *resource)
 {
 
-    char buffer[4096];
-    static char *fmt =
-        "= window label \"Lookup address\"\n"
-        "+ header label \"Lookup address\"\n"
-        "+ field id \"url\" label \"URL\" data \"%s\"\n"
-        "+ button label \"Lookup\" link \"navi://lookup\" \"text/alfi\" mode \"on\"\n"
-        "+ subheader label \"Quick links\"\n"
-        "+ anchor label \"blunder.se\" link \"http://www.blunder.se/\" \"text/alfi\"\n"
-        "+ anchor label \"example\" link \"file:///usr/share/navi/example.alfi\" \"text/alfi\"\n";
-
-    resource->count = sprintf(buffer, fmt, "http://");
-    resource->data = malloc(resource->count);
-
-    strcpy(resource->data, buffer);
-
-    return resource->count;
+    return !strncmp(resource->urlinfo.url, "http:", 5) || !strncmp(resource->urlinfo.url, "https:", 6);
 
 }
 
-static unsigned int navierror(struct resource *resource, unsigned int count, void *data)
+static unsigned int _curl_load(struct resource *resource, unsigned int count, void *data)
 {
 
-    char buffer[4096];
-    static char *fmt =
-        "= window label \"Internal error\"\n"
-        "+ header label \"Internal error\"\n"
-        "+ text label \"An internal error occured.\"\n";
-
-    resource->count = sprintf(buffer, fmt);
-    resource->data = malloc(resource->count);
-
-    strcpy(resource->data, buffer);
-
-    return resource->count;
-
-}
-
-static unsigned int navilookup(struct resource *resource, unsigned int count, void *data)
-{
+    char *q[12];
 
     if (count)
     {
 
-        char *args = data;
-        char *url = args + 4;
-        char *q[3];
+        q[0] = "curl";
+        q[1] = "-s";
+        q[2] = "-A";
+        q[3] = "Navi/1.0";
+        q[4] = "-X";
+        q[5] = "POST";
+        q[6] = "-H";
+        q[7] = "Content-Type: application/x-www-form-urlencoded";
+        q[8] = "-d";
+        q[9] = data;
+        q[10] = resource->urlinfo.url;
+        q[11] = 0;
 
-        url_set(&resource->urlinfo, url);
 
-        q[0] = "navi-resolve";
-        q[1] = resource->urlinfo.url;
-        q[2] = 0;
+    }
 
-        return resolve(resource, "navi-resolve", q);
+    else
+    {
+
+        q[0] = "curl";
+        q[1] = "-s";
+        q[2] = "-A";
+        q[3] = "Navi/1.0";
+        q[4] = "-X";
+        q[5] = "GET";
+        q[6] = resource->urlinfo.url;
+        q[7] = 0;
+
+    }
+
+    return resolve(resource, "curl", q);
+
+}
+
+static unsigned int _navi_match(struct resource *resource)
+{
+
+    return !strncmp(resource->urlinfo.url, "navi:", 5);
+
+}
+
+static unsigned int _navi_load(struct resource *resource, unsigned int count, void *data)
+{
+
+    char *path = resource->urlinfo.url + 7;
+
+    if (!strncmp(path, "blank", 5))
+    {
+
+        char buffer[4096];
+        static char *fmt =
+            "= window label \"Lookup address\"\n"
+            "+ header label \"Lookup address\"\n"
+            "+ field id \"url\" label \"URL\" data \"%s\"\n"
+            "+ button label \"Lookup\" link \"navi://lookup\" \"text/alfi\" mode \"on\"\n"
+            "+ subheader label \"Quick links\"\n"
+            "+ anchor label \"blunder.se\" link \"http://www.blunder.se/\" \"text/alfi\"\n"
+            "+ anchor label \"example\" link \"file:///usr/share/navi/example.alfi\" \"text/alfi\"\n";
+
+        resource->count = sprintf(buffer, fmt, "http://");
+        resource->data = malloc(resource->count);
+
+        strcpy(resource->data, buffer);
+
+        return resource->count;
+
+    }
+
+    else if (!strncmp(path, "error", 5))
+    {
+
+        char buffer[4096];
+        static char *fmt =
+            "= window label \"Internal error\"\n"
+            "+ header label \"Internal error\"\n"
+            "+ text label \"An internal error occured.\"\n";
+
+        resource->count = sprintf(buffer, fmt);
+        resource->data = malloc(resource->count);
+
+        strcpy(resource->data, buffer);
+
+        return resource->count;
+
+    }
+
+    else if (!strncmp(path, "lookup", 6))
+    {
+
+        char *urlparam = data;
+
+        url_set(&resource->urlinfo, urlparam + 4);
+
+        return resource_load(resource, 0, 0);
 
     }
 
@@ -136,80 +188,36 @@ static unsigned int navilookup(struct resource *resource, unsigned int count, vo
 
 }
 
+static unsigned int _external_match(struct resource *resource)
+{
+
+    return 1;
+
+}
+
+static unsigned int _external_load(struct resource *resource, unsigned int count, void *data)
+{
+
+    char *q[4];
+
+    q[0] = "navi-resolve";
+    q[1] = resource->urlinfo.url;
+    q[2] = data;
+    q[3] = 0;
+
+    return resolve(resource, "navi-resolve", q);
+
+}
+
 unsigned int resource_load(struct resource *resource, unsigned int count, void *data)
 {
 
-    if (!strncmp(resource->urlinfo.url, "navi:", 5))
-    {
-
-        char *path = resource->urlinfo.url + 7;
-
-        if (!strncmp(path, "blank", 5))
-            return naviblank(resource, count, data);
-        else if (!strncmp(path, "error", 5))
-            return navierror(resource, count, data);
-        else if (!strncmp(path, "lookup", 6))
-            return navilookup(resource, count, data);
-        else
-            return 0;
-
-    }
-
-    else if (!strncmp(resource->urlinfo.url, "http:", 5) || !strncmp(resource->urlinfo.url, "https:", 6))
-    {
-
-        char *q[12];
-
-        if (count)
-        {
-
-            q[0] = "curl";
-            q[1] = "-s";
-            q[2] = "-A";
-            q[3] = "Navi/1.0";
-            q[4] = "-X";
-            q[5] = "POST";
-            q[6] = "-H";
-            q[7] = "Content-Type: application/x-www-form-urlencoded";
-            q[8] = "-d";
-            q[9] = data;
-            q[10] = resource->urlinfo.url;
-            q[11] = 0;
-
-
-        }
-
-        else
-        {
-
-            q[0] = "curl";
-            q[1] = "-s";
-            q[2] = "-A";
-            q[3] = "Navi/1.0";
-            q[4] = "-X";
-            q[5] = "GET";
-            q[6] = resource->urlinfo.url;
-            q[7] = 0;
-
-        }
-
-        return resolve(resource, "curl", q);
-
-    }
-
-    else
-    {
-
-        char *q[4];
-
-        q[0] = "navi-resolve";
-        q[1] = resource->urlinfo.url;
-        q[2] = data;
-        q[3] = 0;
-
-        return resolve(resource, "navi-resolve", q);
-
-    }
+    if (_navi_match(resource))
+        return _navi_load(resource, count, data);
+    else if (_curl_match(resource))
+        return _curl_load(resource, count, data);
+    else if (_external_match(resource))
+        return _external_load(resource, count, data);
 
     return 0;
 
